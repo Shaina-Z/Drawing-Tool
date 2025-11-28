35e06e1e44c7d913a89352956b7dba9b112c54c2
Shaina <shainaz@udel.edu>
2025-11-28 18:27:23 -0500

    added composable function outlines for tool panel and drawing canvas

--- app/src/main/java/com/example/drawingtool/MainActivity.kt
+++ app/src/main/java/com/example/drawingtool/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.example.drawingtool
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.drawingtool.ui.theme.DrawingToolTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +28,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DrawingToolTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ToolPanel(){
 }
+@Composable
+fun DrawingCanvas(){}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DrawingPreview() {
     DrawingToolTheme {
-        Greeting("Android")
+        DrawingCanvas()
+        ToolPanel()
     }
 }
